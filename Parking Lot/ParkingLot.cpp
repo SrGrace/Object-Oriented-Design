ac894e0e405b694692e01530b205b598dcb273e5
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2017-12-18 00:28:52 +0530

    Update ParkingLot.cpp

--- Parking Lot/ParkingLot.cpp
+++ Parking Lot/ParkingLot.cpp
@@ -1,6 +1,45 @@
 
 
+enum VehicleSize
+{
+    Motorcycle, Compact, Large    
+}
+
+
+class Vehicle 
+{
+    
+}
+
+class Bus : public Vehicle 
+{
+    
+}
+
+class Car : public Vehicle 
+{
+
+}
+
+class Motorcycle : public Vehicle 
+{
+
+}
+
+
 class ParkingLot
 {
     
 }
+
+
+class Level 
+{
+    
+} 
+
+class ParkingSpot 
+{
+    
+}
+
